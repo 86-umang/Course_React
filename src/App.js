9865f24b038bdcc847e2bcddef9f874a552b3eec
Umangkumar Patel <umangkumar.18beceg031@gmail.com>
2022-03-30 09:03:43 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Main from './components/MainComponent';
 import { DISHES } from './shared/dishes';
@@ -15,9 +16,11 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
